c3a9a990426df7424759a2a5ee53b6a0c6db0252
liu shiwei <liushiwei@gmail.com>
2022-11-08 19:35:28 +0800

    build error ， t0402 test error

---
+++ version.h
@@ -0,0 +1,8 @@
+/* Date part of package version string. */
+#define PACKAGE_DATE_VERSION "20210706"
+
+/* Dotted part of package version string. */
+#define PACKAGE_DOTTED_VERSION "1.10.2"
+
+/* Current git commit. */
+#define PACKAGE_GIT_COMMIT "3d2f02156c0e490c34146ed9fa6f18f195a15d72"
